f55267e07d869c4a681c385d4de62ca4eb6c6c02
helloworld <ura22051995@gmail.com>
2023-04-17 14:01:11 +0300

    Создание приложения и его тестирование с помощью Mockito

---
+++ src/main/java/com/example/mockitotestinghomework/MockitoTestingHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockitotestinghomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoTestingHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoTestingHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockitotestinghomework/MockitoTestingHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockitotestinghomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoTestingHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
