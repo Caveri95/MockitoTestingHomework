07a08096ab093b4246f842361c4cbacdf201baa2
helloworld <ura22051995@gmail.com>
2023-04-20 11:59:25 +0300

    После правок

--- src/main/java/com/example/mockitotestinghomework/services/UserServiceImpl.java
+++ src/main/java/com/example/mockitotestinghomework/services/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.mockitotestinghomework.services;
 
 import com.example.mockitotestinghomework.model.User;
-import com.example.mockitotestinghomework.repository.UserDaoImpl;
+import com.example.mockitotestinghomework.repository.UserDao;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoImpl userDao = new UserDaoImpl();
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public boolean checkUserExist(User user) {
